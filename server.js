e39cba680861947381b17dd09e71e60bcd109659
Jacob Luckie <jacobluckie@Jacobs-MacBook-Pro.local>
2018-05-14 19:38:46 +1200

    Project page correction

--- server.js
+++ server.js
@@ -50,8 +50,8 @@ app.get('/about', (req,res) => {
 });
 
 app.get('/projects', (req,res) => {
-    res.render('about.hbs', {
-        pageTitle: 'About'
+    res.render('projects.hbs', {
+        pageTitle: 'Projects'
     });
 });
 
